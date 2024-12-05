e2f496f170a3fc3766d6a790afc8c2e228201ae4
kokornika <160030659+kokornika@users.noreply.github.com>
2024-12-05 13:28:42 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,13 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 });
